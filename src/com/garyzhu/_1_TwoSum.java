279a3dabd2fa4376c8490b7b11c2201cc5cb3aef
zhubaohui <zhubaohui@meituan.com>
2016-09-09 16:44:10 +0800

    day 1

--- src/com/garyzhu/_1_TwoSum.java
+++ src/com/garyzhu/_1_TwoSum.java
@@ -1,27 +1,32 @@
 package com.garyzhu;
 
 import java.util.HashMap;
+import java.util.Map;
 
-/*
-Given an array of integers, find two numbers such that they add up to a specific target number.
-
-The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
-
-You may assume that each input would have exactly one solution.
-
-Input: numbers={2, 7, 11, 15}, target=9
-Output: index1=1, index2=2
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * <p/>
+ * You may assume that each input would have exactly one solution.
+ * <p/>
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * <p/>
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ * UPDATE (2016/2/13):
+ * The return format had been changed to zero-based indices. Please read the above updated description carefully.
  */
 public class _1_TwoSum {
-    public int[] twoSum(int[] numbers, int target) {
-        HashMap<Integer, Integer> helper = new HashMap<Integer, Integer>();
-        for (int i = 0; i < numbers.length; i++) {
-            int x = numbers[i];
-            if (helper.containsKey(target - x)) {
-                return new int[] { helper.get(target - x) + 1, i + 1 };
-            }
-            helper.put(x, i);
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> helper = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            helper.put(nums[i], i);
         }
-        throw new IllegalArgumentException();
+        for (int i = 0; i < nums.length; i++) {
+            if (helper.containsKey(target - nums[i]) && (i != helper.get(target - nums[i]))) {
+                return new int[] {i, helper.get(target - nums[i])};
+            }
+        }
+        return null;
     }
 }
---
+++ src/com/garyzhu/_263_UglyNumber.java
@@ -0,0 +1,37 @@
+package com.garyzhu;
+
+/**
+ * Write a program to check whether a given number is an ugly number.
+ * <p/>
+ * Ugly numbers are positive numbers whose prime factors only include 2, 3, 5.
+ * <p/>
+ * For example, 6, 8 are ugly while 14 is not ugly since it includes another prime factor 7.
+ * <p/>
+ * Note that 1 is typically treated as an ugly number.
+ * <p/>
+ * Tags: Math
+ * <p/>
+ * Similar Problems: (E) Happy Number (E) Count Primes (M) Ugly Number II
+ */
+public class _263_UglyNumber {
+    public boolean isUgly(int num) {
+        if (num <= 0) {
+            return false;
+        }
+
+        while (num % 2 == 0) {
+            num /= 2;
+        }
+        while (num % 3 == 0) {
+            num /= 3;
+        }
+        while (num % 5 == 0) {
+            num /= 5;
+        }
+        if (num == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
